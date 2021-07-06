b9922c23f0a568c93585e1153628e1180f715c28
MOHAMMED TALHA <atalhahb@gmail.com>
2021-07-06 23:01:24 +0530

    Updates

---
+++ precache-manifest.2e69cbffdb7660fb709cb2cb2d64da2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "20acacde423637f49c0b",
+    "url": "/News/static/css/main.9679018d.chunk.css"
+  },
+  {
+    "revision": "20acacde423637f49c0b",
+    "url": "/News/static/js/main.ad828e52.chunk.js"
+  },
+  {
+    "revision": "2a1ceda6629da20cf8c6",
+    "url": "/News/static/js/runtime~main.0513ca97.js"
+  },
+  {
+    "revision": "5d5f65ad1747e845f7e5",
+    "url": "/News/static/js/2.0fb8701b.chunk.js"
+  },
+  {
+    "revision": "ee609f01ae985e2ba079",
+    "url": "/News/static/js/3.af959e76.chunk.js"
+  },
+  {
+    "revision": "6526d89a541b495e8f4f632d8f3d62a4",
+    "url": "/News/index.html"
+  }
+];
